151d80cb0c1e41c4e61f635e7d2099bdb4302f4e
Your Name <kalyadin.maksim@mail.ru>
2020-05-19 12:21:32 +0400

    fix_2

--- src/oclab3/Main.java
+++ src/oclab3/Main.java
@@ -7,14 +7,28 @@ public class Main {
 	public static void main(String[] args) {
 		int index;
 		Unit U = new Unit(2048, 256);
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            System.out.println("Введите индекс страницы в таблице страниц :");
-            index = scanner.nextInt();
-            if (index >= 0 && index < U.getSizePageTable()){
-                U.addressToPage(index);
-            }
-        }
+        U.addressToPage(1);
+        U.addressToPage(101);
+        U.addressToPage(45);
+        U.addressToPage(46);
+        U.addressToPage(80);
+        U.addressToPage(94);
+        U.addressToPage(90);
+        U.addressToPage(3);
+        U.addressToPage(60);
+        U.addressToPage(97);
+        U.addressToPage(29);
+        U.addressToPage(50);
+        U.addressToPage(68);
+        U.addressToPage(6);
+        U.addressToPage(92);
+        U.addressToPage(31);
+        U.addressToPage(79);
+        U.addressToPage(51);
+        U.addressToPage(73);
+        U.addressToPage(88);
+        U.addressToPage(20);
+        U.addressToPage(44);
 	}
 
 }
